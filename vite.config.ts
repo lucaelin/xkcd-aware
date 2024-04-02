ae5bc601f11c81c80aafe2de4d5d56687c8716e0
Luca Haneklau <git@luca.lsys.ac>
2024-04-02 22:34:45 +0200

    fix: vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ const crossOriginIsolation = () => ({
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte(), crossOriginIsolation()],
+  base: "/xkcd-aware/",
   server: {
     hmr: false,
   },
